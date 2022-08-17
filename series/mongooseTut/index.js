38f8d1997529b127d44e829232217cf24ddc08dc
ArmaanCode2 <armaansecond9@gmail.com>
2022-08-17 21:40:46 +0530

    completed till 86

--- series/mongooseTut/index.js
+++ series/mongooseTut/index.js
@@ -3,7 +3,33 @@ mongoose.connect('mongodb://localhost/armaanKart',{useNewUrlParser: true,useUniF
 
 var db = mongoose.connection;
 db.on('error',console.error.bind(console,'connection error'));
-db.once('open',function(){
-    //connected
-    console.log("connected to db");
+// db.once('open',function(){
+//     //connected
+//     console.log("connected to db");
+// });
+
+const kittySchema = new mongoose.Schema({
+    name: String
 });
+
+kittySchema.methods.speak = function speak() {
+    const greeting ="Meow name is " + this.name;
+    // console.log(greeting);
+  };
+  
+
+const Kitten = mongoose.model('armaanKitty', kittySchema);
+
+const armaanKitty = new Kitten({ name: 'armaanKitty' });
+// console.log(armaanKitty.name);
+// armaanKitty.speak();
+
+armaanKitty.save(function(err,armaanKitty){
+    if(err) return console.err(err);
+    // armaanKitty.speak();
+})
+
+Kitten.find({name:"armaanKitty"},function(err,armaanKitty){
+    if(err) return console.err(err);
+    console.log(armaanKitty);
+})
