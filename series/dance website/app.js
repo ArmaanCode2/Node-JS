7c6178c62d07559558e8fe0afe233f962b4e1e27
ArmaanCode2 <armaansecond9@gmail.com>
2022-08-28 01:01:01 +0530

    completed till 88 and completed chatapp

--- series/dance website/app.js
+++ series/dance website/app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/contactDance',{useNewUrlParser: true});
 const port = 80;
+
+//mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String,
+});
+
+const contact = mongoose.model('contact', contactSchema);
 
 app.use('/static',express.static('static'));//serving static files
 app.use(express.urlencoded());
@@ -19,6 +33,17 @@ app.get('/',(req,res)=>{
 app.get('/contact',(req,res)=>{
     const params = {}
     res.status(200).render('contact.pug',params);
+});
+
+app.post('/contact',(req,res)=>{
+    var myData = new contact(req.body);
+    myData.save().then(()=>{
+        res.send("This Information has been dilivered to our Team");
+    }).catch(()=>{
+        res.status(400).send("Information Could not be dilivered Please Try Again");
+    })
+
+    // res.status(200).render('contact.pug');
 });
 
 
